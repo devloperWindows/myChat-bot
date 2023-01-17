7b2bd1d7cf0d95c27791d68e136a5aa5806f1b72
devloperWindows <122878302+devloperWindows@users.noreply.github.com>
2023-01-17 16:52:18 +0530

    api key update

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai'
 dotenv.config()
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
